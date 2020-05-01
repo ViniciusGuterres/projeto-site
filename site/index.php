7bb7898ed395c104d486c9dc2206153858f7bd29
Vinicius Guterres <viniguter@gmail.com>
2020-05-01 17:28:08 -0300

    add index
    add main index and some a html articles

---
+++ site/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M4 Cordas</title>
+</head>
+<body>
+    
+   <main>
+    
+        <header>
+
+            <article>
+                <h1>Title</h1>
+            </article>
+
+            <article>
+                <h1>Artigo 001</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam assumenda fuga veritatis repudiandae error reiciendis hic iure cumque voluptatum ea cum sapiente nesciunt deleniti perferendis nobis necessitatibus, facere magnam. Amet.</p>
+            </article>
+
+            <article>
+                <h1>Artigo 002</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, iusto! Sapiente quibusdam illum aliquam quas nam voluptatibus a soluta nulla? Fugit ullam itaque tenetur iure quod, molestias quo. Nisi, quos?</p>
+            </article>
+
+        </header>
+   </main>
+
+
+
+</body>
+</html>
